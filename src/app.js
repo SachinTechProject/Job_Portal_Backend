a64be9c1c426f46cd7d04477a710712186a95b6d
SachinTechProject <148031974+SachinTechProject@users.noreply.github.com>
2026-03-19 08:27:08 +0530

    added the frontend url

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import cors from 'cors';
 const app = express();
 app.use(cors(
     {
-        origin: "http://localhost:5173",
+        origin: "https://job-portal-frontend-ecru-chi.vercel.app",
         credentials: true,
     }
 ))
